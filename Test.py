5cbdcd35b0b5189abe17cf6ccbb7b68bd573fb1d
Brady Groharing <bradygroharing@Fishs-MBP-2.wifi.te>
2017-09-13 13:46:17 -0600

    Added a couple examples of how to use PRAW

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 
 import praw as praw
+from praw.models import MoreComments
+
 import requests
 import json
 
@@ -11,4 +13,31 @@ reddit = praw.Reddit(client_id='El479iqdfj-v0g',
                      username='EverestAtlas')
 
 print(reddit.user.me())
+
+
+subredditIds = []
+
+# This section grabs info from the subreddit /r/funny and displays info about it
+funny = reddit.subreddit('funny')
+for submission in funny.hot(limit=25):
+    print(submission.title)
+    print(submission.score)
+    print(submission.id)
+    subredditIds.append(submission.id)
+    print(submission.url)
+    print(submission.author)
+    print("\n")
+
+# This section goes through all the posts I've gotten from above and grabs the top level comments from each one
+print("Comments Here: ")
+for id in subredditIds:
+    submission = reddit.submission(id=id)
+    for top_level_comment in submission.comments:
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        print(top_level_comment.body)
+    print("END OF CURRENT COMMENT TREE\n")
+
+
+
 
